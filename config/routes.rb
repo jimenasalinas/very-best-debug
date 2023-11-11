7660f0dc51b824eaeb5620dc39f2bc2466b3d0c1
Jimena Salinas <111835409+jimenasalinas@users.noreply.github.com>
2023-11-11 20:13:20 +0000

    debugging venue

--- config/routes.rb
+++ config/routes.rb
@@ -7,8 +7,8 @@ Rails.application.routes.draw do
   post("/update_users/:user_id", { :controller => "users", :action => "update" })
 
   get("/venues", { :controller => "venues", :action => "index" })
-  post("/insert_venue_record", { :controller => "venues", :action => "create" })
-  get("/venue/:an_id", { :controller => "venue", :action => "show" })
+  get("/insert_venue_record", { :controller => "venues", :action => "create" })
+  get("/venues/:an_id", { :controller => "venue", :action => "show" })
   post("/update_venue/:the_id", { :controller => "application", :action => "update" })
   get("/delete_venue/:id_to_delete", { :controller => "venue", :action => "destroy" })
   
